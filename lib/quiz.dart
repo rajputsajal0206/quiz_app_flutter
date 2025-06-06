e7bd1998b80ef57c4cb206b14f0a9983b383f74c
Shitanshu Sajal <singhshitanshu.0206@gmail.com>
2025-06-06 12:56:56 +0530

    initState, setState and passing function as variable handled

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/question_screen.dart';
 import 'start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -8,7 +9,28 @@ class Quiz extends StatefulWidget {
   State<Quiz> createState() => _QuizState();
 }
 
+//_QUIZSTATE class confirms that this is private and can be used only in this quiz.dart
 class _QuizState extends State<Quiz> {
+
+  //the below statement confirms that activeScreen widget can be null or can have a widget as a value
+  Widget? activeScreen;
+
+
+  //initState method is called before execution of build method and post the memory allocation to objects and variables
+  @override
+  void initState() {
+    //in StartScreen we are passing function as argument and without paranthesis()
+    // Not calling with paranthesis as it will execute it
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionScreen();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,18 +40,23 @@ class _QuizState extends State<Quiz> {
         //CONTAINER widgets don't occupy any space of it's own, rather it takes space as per it's child
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
+              gradient: LinearGradient(
+            colors: [
               Color.fromARGB(255, 78, 13, 151),
               Color.fromARGB(255, 107, 15, 168),
             ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            )
-          ),
-            //We are calling StartScreen with const keyword because when we defined the class
-            //we also declared a constructor for the Class with const keyword
-            child: const StartScreen()),
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+          //We are calling StartScreen with const keyword because when we defined the class
+          //we also declared a constructor for the Class with const keyword
+          child: activeScreen,
+        ),
       ),
     );
   }
 }
+
+// setState(() {
+//
+// });
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz,{super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +42,7 @@ class StartScreen extends StatelessWidget {
             height: 40,
           ),
           OutlinedButton.icon(
-            onPressed: (){},
+            onPressed: startQuiz,
             icon: const Icon(Icons.arrow_right_alt),
             //In place of text we use label to give title to the button
             label: const Text('Start Now',
