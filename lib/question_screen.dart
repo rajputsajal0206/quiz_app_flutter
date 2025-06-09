82ae883f4b935b1a0ed8a6e5a3601f1dc1443353
Shitanshu Sajal <singhshitanshu.0206@gmail.com>
2025-06-09 17:22:41 +0530

    map and spread functions are used

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app_flutter/answer_button.dart';
+import 'package:quiz_app_flutter/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -13,18 +15,41 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const SizedBox(
+    final currentQuestion = questionsList[0];
+
+    return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children:<Widget> [
-          Text('The Question is ...',
+        children: <Widget>[
+          Text(
+            currentQuestion.question,
             maxLines: 2,
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.white,
               fontSize: 24,
             ),
-          )
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ...currentQuestion.multipleOptions.map((answer) {
+            return ElevatedAnswerOption(
+              answerText: answer,
+              onClick: () {},
+            );
+          }),
+          //here "multipleOptions" is a list amd every item of this list will be passed to map function as an argument one by one
+          //the function defined in map is executed on every value passed as argument in map function
+          // map will accept values of list and perform actions on it and return a new list
+          //... is used because column accepts only List of Widget but map tries to add list to a list so spread(...) will extract the value from list
+
+          // currentQuestion.multipleOptions.map((answer){
+          //   return ElevatedAnswerOption(
+          //     answerText: answer,
+          //     onClick: () {},
+          //   ),
+          // })
         ],
       ),
     );
